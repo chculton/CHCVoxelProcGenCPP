cba04033f95b965769ed959effaa8174219612d1
chculton <chansculton@gmail.com>
2018-02-12 21:53:39 +1300

    Bug Smashing...

--- CHCVoxelProcGenCPP/Source/CHCVoxelProcGenCPP/CHCVoxelProcGenCPP.Build.cs
+++ CHCVoxelProcGenCPP/Source/CHCVoxelProcGenCPP/CHCVoxelProcGenCPP.Build.cs
@@ -5,6 +5,17 @@ using UnrealBuildTool;
 
 public class CHCVoxelProcGenCPP : ModuleRules
 {
+    private string ModulePath
+    {
+        get { return ModuleDirectory; }
+    }
+
+    // The path to our Third Party code assets; e.g. ProjectFolder/ThirdParty/
+    private string ThirdPartyPath
+    {
+        get { return Path.Combine(ModulePath, "../../ThirdParty/"); }
+    }
+
     public CHCVoxelProcGenCPP(ReadOnlyTargetRules Target) : base(Target)
     {
         PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
@@ -20,118 +31,39 @@ public class CHCVoxelProcGenCPP : ModuleRules
         // PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
         // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-        private string ModulePath
-    {
-        get { return ModuleDirectory; }
-    }
-
-    // The path to our Third Party code assets; e.g. ProjectFolder/ThirdParty/
-    private string ThirdPartyPath
-    {
-        get { return Path.Combine(ModulePath, ".D:/Users/chans/Documents/Unreal Projects/CHCVoxelProcGenCPP/CHCVoxelProcGenCPP/ThirdParty/"); }
-    }
-
-   /* public VoxelTerrain(TargetInfo Target)
-    {
-        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
-
-        PrivateDependencyModuleNames.AddRange(new string[] { });*/
+        // The path to our game module; e.g.ProjectFolder / Source / ModuleName /
 
         ////////////////////// Custom Voxel Terrain Stuff Starts Here //////////////////////////////////////
         // You will need to compile and add additional libraries if you want to use this on platforms not listed below!
-    switch (Target.Platform)
-    {
-        // 64-bit Windows
-        case UnrealTargetPlatform.Win64:
-            PublicAdditionalLibraries.Add(Path.Combine(ThirdPartyPath, "accidental-noise-library", "build", "ANL", "x64", "ANL.lib"));
-            break;
-    
-        // 32-bit Windows
-        case UnrealTargetPlatform.Win32:
-            PublicAdditionalLibraries.Add(Path.Combine(ThirdPartyPath, "accidental-noise-library", "build", "ANL", "x86", "ANL.lib"));
-            break;
-    
-        // Mac
-        case UnrealTargetPlatform.Mac:
-            PublicAdditionalLibraries.Add(Path.Combine(ThirdPartyPath, "accidental-noise-library", "build", "ANL", "Universal", "libANL.a"));
-            break;
-    
-        // Linux
-        case UnrealTargetPlatform.Linux:
-            PublicAdditionalLibraries.Add(Path.Combine(ThirdPartyPath, "accidental-noise-library", "build", "ANL", "x64", "libANL.a"));
-            break;
-    
-        default:
-            break;
-    }
+        switch (Target.Platform)
+        {
+            // 64-bit Windows
+            case UnrealTargetPlatform.Win64:
+                PublicAdditionalLibraries.Add(Path.Combine(ThirdPartyPath, "accidental-noise-library", "build", "ANL", "x64", "ANL.lib"));
+                break;
 
-// Include the headers for PolyVox and ANL so we can access them later.
-    PublicIncludePaths.Add(Path.Combine(ThirdPartyPath, "polyvox/include"));
-    PublicIncludePaths.Add(Path.Combine(ThirdPartyPath, "accidental-noise-library"));
-        ////////////////////// End Voxel Terrain Stuff //////////////////////////////////////////////////////
-    }
-    //Voxel Code
-    /*private string ModulePath
-    {
-        //get { return Path.GetDirectoryName(RulesCompiler.GetModuleFilename(this.GetType().Name)); }
-        get { return ModuleDirectory; }
-    }
+            // 32-bit Windows
+            case UnrealTargetPlatform.Win32:
+                PublicAdditionalLibraries.Add(Path.Combine(ThirdPartyPath, "accidental-noise-library", "build", "ANL", "x86", "ANL.lib"));
+                break;
 
-    // The path to our Third Party code assets; e.g. ProjectFolder/ThirdParty/
-    private string ThirdPartyPath
-    {
-        //get { return Path.GetFullPath(Path.Combine(ModulePath, "../../ThirdParty/")); }
-        get { return Path.Combine(ModuleDirectory, "../../ThirdParty/"); }
-    }*/
+            // Mac
+            case UnrealTargetPlatform.Mac:
+                PublicAdditionalLibraries.Add(Path.Combine(ThirdPartyPath, "accidental-noise-library", "build", "ANL", "Universal", "libANL.a"));
+                break;
 
-    // The path to our game module; e.g. ProjectFolder/Source/ModuleName/
-   /* private string ModulePath
-    {
-        get { return ModuleDirectory; }
-    }
+            // Linux
+            case UnrealTargetPlatform.Linux:
+                PublicAdditionalLibraries.Add(Path.Combine(ThirdPartyPath, "accidental-noise-library", "build", "ANL", "x64", "libANL.a"));
+                break;
 
-    // The path to our Third Party code assets; e.g. ProjectFolder/ThirdParty/
-    private string ThirdPartyPath
-    {
-        get { return Path.Combine(ModulePath, ".D:/Users/chans/Documents/Unreal Projects/CHCVoxelProcGenCPP/CHCVoxelProcGenCPP/ThirdParty/"); }
-    }
-
-   /* public VoxelTerrain(TargetInfo Target)
-    {
-        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
-
-        PrivateDependencyModuleNames.AddRange(new string[] { });*/
-
-        ////////////////////// Custom Voxel Terrain Stuff Starts Here //////////////////////////////////////
-        // You will need to compile and add additional libraries if you want to use this on platforms not listed below!
-    /*switch (Target.Platform)
-    {
-        // 64-bit Windows
-        case UnrealTargetPlatform.Win64:
-            PublicAdditionalLibraries.Add(Path.Combine(ThirdPartyPath, "accidental-noise-library", "build", "ANL", "x64", "ANL.lib"));
-            break;
-    
-        // 32-bit Windows
-        case UnrealTargetPlatform.Win32:
-            PublicAdditionalLibraries.Add(Path.Combine(ThirdPartyPath, "accidental-noise-library", "build", "ANL", "x86", "ANL.lib"));
-            break;
-    
-        // Mac
-        case UnrealTargetPlatform.Mac:
-            PublicAdditionalLibraries.Add(Path.Combine(ThirdPartyPath, "accidental-noise-library", "build", "ANL", "Universal", "libANL.a"));
-            break;
-    
-        // Linux
-        case UnrealTargetPlatform.Linux:
-            PublicAdditionalLibraries.Add(Path.Combine(ThirdPartyPath, "accidental-noise-library", "build", "ANL", "x64", "libANL.a"));
-            break;
-    
-        default:
-            break;
-    }
+            default:
+                break;
+        }
 
         // Include the headers for PolyVox and ANL so we can access them later.
-    PublicIncludePaths.Add(Path.Combine(ThirdPartyPath, "polyvox/include"));
-    PublicIncludePaths.Add(Path.Combine(ThirdPartyPath, "accidental-noise-library"));
-        ////////////////////// End Voxel Terrain Stuff //////////////////////////////////////////////////////*/
+        PublicIncludePaths.Add(Path.Combine(ThirdPartyPath, "polyvox/include"));
+        PublicIncludePaths.Add(Path.Combine(ThirdPartyPath, "accidental-noise-library"));
+        ////////////////////// End Voxel Terrain Stuff //////////////////////////////////////////////////////
+    }
 }
